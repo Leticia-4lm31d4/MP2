0098322fc9a65867bf9c56a5fb990149e50e70f4
Leticia Almeida <leticiapaulinoferreira@gmail.com>
2024-02-05 20:07:36 -0300

    mvc

--- mp2/src/main/webapp/scripts/graficos.js
+++ mp2/src/main/webapp/scripts/graficos.js
@@ -4,51 +4,54 @@ google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(desenharGraficoPizza);
 
 function desenharGraficoPizza() {
-    var data = google.visualization.arrayToDataTable([
-        ['Categoria', 'Porcentagem'],
-        ['Atrasados', 30],
-        ['Pouco Atrasados', 20],
-        ['No Horário', 50]
-    ]);
+    fetch('url-do-seu-servlet-de-atrasos')
+        .then(response => response.json())
+        .then(data => {
+            var dataTable = new google.visualization.DataTable();
+            dataTable.addColumn('string', 'Categoria');
+            dataTable.addColumn('number', 'Porcentagem');
+            data.forEach(item => {
+                dataTable.addRow([item.categoria, item.porcentagem]);
+            });
 
-    var options = {
-        //title: 'Distribuição de Atrasos dos Ônibus',
-        pieHole: 0.25,
-    };
+            var options = {
+                //title: 'Distribuição de Atrasos dos Ônibus',
+                pieHole: 0.25,
+            };
 
-    var chart = new google.visualization.PieChart(document.getElementById('grafico-pizza'));
-    chart.draw(data, options);
+            var chart = new google.visualization.PieChart(document.getElementById('grafico-pizza'));
+            chart.draw(dataTable, options);
+        });
 }
 
 // Lotação dos ônibus :: Gráfico de Linhas
 google.charts.setOnLoadCallback(desenharGraficoBarras);
 
 function desenharGraficoBarras() {
-    var data = google.visualization.arrayToDataTable([
-        ['Horário', 'Lotação Média'],
-        ['7:00', 60],
-        ['9:00', 32],
-        ['11:00', 50],
-        ['14:00', 25],
-        ['16:00', 18],
-        ['18:00', 60],
-        ['20:00', 46],
-        ['22:00', 15]
-    ]);
+    fetch('url-do-seu-servlet-de-lotacao')
+        .then(response => response.json())
+        .then(data => {
+            var dataTable = new google.visualization.DataTable();
+            dataTable.addColumn('string', 'Horário');
+            dataTable.addColumn('number', 'Lotação Média');
+            data.forEach(item => {
+                dataTable.addRow([item.horario, item.lotacaoMedia]);
+            });
 
-    var options = {
-        //title: 'Lotação Média dos Ônibus por Horário',
-        chartArea: {width: '60%'},
-        hAxis: {
-            title: 'Lotação Média',
-            minValue: 0,
-            maxValue: 60
-        },
-        vAxis: {
-            title: 'Horário'
-        }
-    };
+            var options = {
+                //title: 'Lotação Média dos Ônibus por Horário',
+                chartArea: {width: '60%'},
+                hAxis: {
+                    title: 'Lotação Média',
+                    minValue: 0,
+                    maxValue: 60
+                },
+                vAxis: {
+                    title: 'Horário'
+                }
+            };
 
-    var chart = new google.visualization.BarChart(document.getElementById('grafico-barras'));
-    chart.draw(data, options);
+            var chart = new google.visualization.BarChart(document.getElementById('grafico-barras'));
+            chart.draw(dataTable, options);
+        });
 }
