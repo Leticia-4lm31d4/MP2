da614277f7fae5134d1dcd597737fac7ebd5c408
Leticia Almeida <leticiapaulinoferreira@gmail.com>
2024-02-05 18:53:44 -0300

    conexão com o BD e criação das pastas para a arq MVC

--- mp2/src/main/java/Hello.java
+++ mp2/src/main/java/Hello.java
@@ -1,5 +1,57 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String str = "conexao não realizada";
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// faz conexão com BD
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			// substitua pelos dados de acesso do seu BD
+			String connection = "jdbc:mysql://localhost/busbus"; // nome do BD: busbus
+			String user = "localhost", pass = "devweb";
+
+			Connection conn = DriverManager.getConnection(connection, user, pass);
+			str = "conexao realizada com sucesso"; // caso conexão seja realizada
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		PrintWriter response_writer = response.getWriter();
+		response_writer.print(this.buildPage(str)); // envia HTML de resposta
+		response_writer.close();
+	} // fim service
+
+	// método auxiliar apenas para montar um retorno
+	private String buildPage(String str) {
+		String page;
+
+		page = "<!doctype html>"
+				+ "<html>" + "<head>" + "<meta charset=\"UTF-8\">" + "<title>Testa Conexao</title>"
+				+ "</head>" + "<body>" + "<h1>Testa conexao</h1>" + "<h2>Conexão: " + str + "!</h2>"
+				+ "</body>" + "</html>";
+
+		return page;
+	}
+
+}
+
+/*import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -20,4 +72,4 @@ public class Hello extends HttpServlet {
 		doGet(request, response);
 	}
 
-}
+}*/
