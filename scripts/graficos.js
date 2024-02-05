8466bc2b55bf08c826b3e8e1356a647c3d529648
Leticia Almeida <leticiapaulinoferreira@gmail.com>
2024-02-05 14:33:18 -0300

    pag dos graficos feita | ainda sem BD

--- scripts/graficos.js
+++ scripts/graficos.js
@@ -5,15 +5,15 @@ google.charts.setOnLoadCallback(desenharGraficoPizza);
 
 function desenharGraficoPizza() {
     var data = google.visualization.arrayToDataTable([
-    ['Categoria', 'Porcentagem'],
-    ['Atrasados', 30],
-    ['Pouco Atrasados', 20],
-    ['No Horário', 50]
+        ['Categoria', 'Porcentagem'],
+        ['Atrasados', 30],
+        ['Pouco Atrasados', 20],
+        ['No Horário', 50]
     ]);
 
     var options = {
-    //title: 'Distribuição de Atrasos dos Ônibus',
-    pieHole: 0.4,
+        //title: 'Distribuição de Atrasos dos Ônibus',
+        pieHole: 0.25,
     };
 
     var chart = new google.visualization.PieChart(document.getElementById('grafico-pizza'));
@@ -25,22 +25,28 @@ google.charts.setOnLoadCallback(desenharGraficoBarras);
 
 function desenharGraficoBarras() {
     var data = google.visualization.arrayToDataTable([
-    ['Horário', 'Lotação Média'],
-    ['8:00', 70],
-    ['12:00', 80],
-    ['18:00', 90]
+        ['Horário', 'Lotação Média'],
+        ['7:00', 60],
+        ['9:00', 32],
+        ['11:00', 50],
+        ['14:00', 25],
+        ['16:00', 18],
+        ['18:00', 60],
+        ['20:00', 46],
+        ['22:00', 15]
     ]);
 
     var options = {
-    //title: 'Lotação Média dos Ônibus por Horário',
-    chartArea: {width: '50%'},
-    hAxis: {
-        title: 'Lotação Média',
-        minValue: 0
-    },
-    vAxis: {
-        title: 'Horário'
-    }
+        //title: 'Lotação Média dos Ônibus por Horário',
+        chartArea: {width: '60%'},
+        hAxis: {
+            title: 'Lotação Média',
+            minValue: 0,
+            maxValue: 60
+        },
+        vAxis: {
+            title: 'Horário'
+        }
     };
 
     var chart = new google.visualization.BarChart(document.getElementById('grafico-barras'));
